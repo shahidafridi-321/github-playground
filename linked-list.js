199d23bc175fd04f4a32b50910e87c6cce15fa02
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-09-17 19:34:04 +0500

    Add append method that insert value at the end of LinkedList..

--- linked-list.js
+++ linked-list.js
@@ -2,6 +2,19 @@ class LinkedList {
 	constructor() {
 		this.head = null;
 	}
+
+	append(value) {
+		let newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			return;
+		}
+		let currentNode = this.head;
+		while (currentNode.next) {
+			currentNode = currentNode.next;
+		}
+		currentNode = newNode;
+	}
 }
 
 class Node {
